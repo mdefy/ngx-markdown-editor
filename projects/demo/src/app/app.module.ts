81c84cdee7186a3e39de28e9fecd92cd13771747
Lenard Funk <lenard.funk@uni-ulm.de>
2020-09-19 11:31:02 +0200

    feat: setup demo for ngx-markdown-editor

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MarkdownEditorModule } from 'projects/ngx-markdown-editor/src/public-api';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MarkdownEditorModule],
   providers: [],
   bootstrap: [AppComponent],
 })

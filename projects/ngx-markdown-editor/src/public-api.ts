5fc23629f1064092143a0ce0cf95058c996629d4
Lenard Funk <lenard.funk@uni-ulm.de>
2021-01-18 15:54:54 +0100

    refactor: export types explicitely + export default toolbar and statusbar

--- projects/ngx-markdown-editor/src/public-api.ts
+++ projects/ngx-markdown-editor/src/public-api.ts
@@ -2,7 +2,20 @@
  * Public API Surface of ngx-markdown-editor
  */
 
+export { DEFAULT_STATUSBAR, getDefaultStatusbarItem, getDefaultStatusbarItems } from './lib/default-statusbar-config';
+export { DEFAULT_TOOLBAR, getDefaultItem, getDefaultItems } from './lib/default-toolbar-config';
 export * from './lib/markdown-editor.component';
 export * from './lib/markdown-editor.module';
-export * from './lib/types';
+export {
+  LanguageTag,
+  NgxMdeIcon,
+  NgxMdeItem,
+  NgxMdeItemDef,
+  NgxMdeItemName,
+  NgxMdeOptions,
+  NgxMdeStatusbarItem,
+  NgxMdeStatusbarItemDef,
+  NgxMdeStatusbarItemName,
+  OptionalI18n,
+} from './lib/types';
 export * from './lib/util/from-cm-event';

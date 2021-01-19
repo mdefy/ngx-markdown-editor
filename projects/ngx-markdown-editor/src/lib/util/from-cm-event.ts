acf73483fbf376d7215f065068491853f6e49383
Lenard Funk <lenard.funk@uni-ulm.de>
2021-01-19 11:01:27 +0100

    fix: make observable emit event arguments as object as defined by type

--- projects/ngx-markdown-editor/src/lib/util/from-cm-event.ts
+++ projects/ngx-markdown-editor/src/lib/util/from-cm-event.ts
@@ -92,9 +92,78 @@ export function fromCmEvent<K extends CodeMirror.DOMEvent & keyof DocumentAndEle
  * @param cm the `CodeMirror.Editor` instance of which the event shall be observed
  * @param eventName the name of a _CodeMirror_ event
  */
-export function fromCmEvent(cm: CodeMirror.Editor, eventName: string): Observable<{ instance: CodeMirror.Editor }> {
+export function fromCmEvent(cm: CodeMirror.Editor, eventName: string): Observable<any> {
   return new Observable((subscriber) => {
-    const handler = (...args: any[]) => subscriber.next(args.length > 1 ? args : args[0]);
+    let handler: () => void;
+    switch (eventName) {
+      case 'change':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], changeObj: args[1] });
+        break;
+      case 'changes':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], changes: args[1] });
+        break;
+      case 'beforeChange':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], changeObj: args[1] });
+        break;
+      case 'cursorActivity':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0] });
+        break;
+      case 'keyHandled':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], name: args[1], event: args[2] });
+        break;
+      case 'inputRead':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], changeObj: args[1] });
+        break;
+      case 'electricInput':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], line: args[1] });
+        break;
+      case 'beforeSelectionChange':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], obj: args[1] });
+        break;
+      case 'viewportChange':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], from: args[1], to: args[2] });
+        break;
+      case 'swapDoc':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], oldDoc: args[1] });
+        break;
+      case 'gutterClick':
+        handler = (...args: any[]) =>
+          subscriber.next({ instance: args[0], line: args[1], gutter: args[2], clickEvent: args[3] });
+        break;
+      case 'gutterContextMenu':
+        handler = (...args: any[]) =>
+          subscriber.next({ instance: args[0], line: args[1], gutter: args[2], contextMenu: args[3] });
+        break;
+      case 'focus':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], event: args[1] });
+        break;
+      case 'blur':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], event: args[1] });
+        break;
+      case 'scroll':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0] });
+        break;
+      case 'refresh':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0] });
+        break;
+      case 'optionChange':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], option: args[1] });
+        break;
+      case 'scrollCursorIntoView':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], event: args[1] });
+        break;
+      case 'update':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0] });
+        break;
+      case 'renderLine':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], line: args[1], element: args[2] });
+        break;
+      case 'overwriteToggle':
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], overwrite: args[1] });
+        break;
+      default:
+        handler = (...args: any[]) => subscriber.next({ instance: args[0], event: args[1] });
+    }
 
     if (cm) {
       cm.on(eventName, handler);
